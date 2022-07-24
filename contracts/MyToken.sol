fdf1ac8a200b04f6692447beb16ddd054d27a530
Mr Stegosaurus <mrstego123@gmail.com>
2022-07-23 21:28:22 -0400

    add deployment script and initial tests

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,6 +1,6 @@
 pragma solidity >= 0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
   constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") public {
