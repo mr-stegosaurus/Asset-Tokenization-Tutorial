95cce245adc03e998d21009d50cc33360c6a0849
Mr Stegosaurus <mrstego123@gmail.com>
2022-07-31 18:10:39 -0400

    update test frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import getWeb3 from "./getWeb3";
 
 import "./App.css";
 class App extends Component {
-  state = { loaded: false };
+  state = { loaded: false, kycAddress: "0x123" };
 
     componentDidMount = async () => {
       try {
@@ -44,20 +44,22 @@ class App extends Component {
         );
         console.error(error);
       }
-    };
+    }
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+    handleInputChange= (event) => {
+      const target = event.target;
+      const value = target.type === "checkbox" ? target.checked : target.value;
+      const name = target.name;
+      this.setState({
+        [name]: value
+      });
+    }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+    handleKycSubmit = async () => {
+      const {kycAddress} = this.state;
+      await this.kycContract.methods.setKycCompleted(kycAddress).send({from: this.accounts[0]});
+      alert("Account "+kycAddress+" is now whitelisted");
+    }
 
   render() {
     if (!this.state.loaded) {
@@ -65,20 +67,16 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Capuccino Token for StarDucks</h1>
+
+        <h2>Enable your account</h2>
+        Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputchange} />
+        <button type="button" onClick={this.handleKycSubmit}>Add Address to Whitelist</button>
       </div>
     );
   }
+
+
 }
 
 export default App;
