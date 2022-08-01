464757ab05a196377ff1d5c9fd71ff0c399c31e5
Mr Stegosaurus <mrstego123@gmail.com>
2022-07-31 22:32:21 -0400

    add buy function

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+require('dotenv').config({path: './.env'})
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
